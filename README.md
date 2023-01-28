c7da17c130d53aa4ec385e06e472dedf83526579
Elizabeth Engelman <elizabeth@redsquirrel.com>
2023-01-28 13:59:35 -0500

    Day 3

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Daily Practice
 * day 1, 2023-01-26: https://www.instagram.com/p/Cn9ubv3g7CL/
 * day 2, 2023-01-27: https://www.instagram.com/p/Cn92oeZgY-I/
+* day 3, 2023-01-28: https://www.instagram.com/p/Cn-MMaogYY1/
+
+
+## Ideas:
+### ml5, posenet, p5js
+* with video on
+* don't overwrite the background
+* different color skeleton & keypoints
+* different markers for the keypoints
+* smiley face for the head
+
+### Other stuff
+* not sure which angle is best... i think its best when the person is facing the camera
+* it's prob best to just film a couple of minutes, it gets boring after too long
+* what else to explore with the posenet lib?
--- sketch.js
+++ sketch.js
@@ -4,8 +4,9 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
+// Resources:
 // https://medium.com/@pallavi.ray24/saving-p5-js-sketches-as-videos-9376068afc10
-
+// https://editor.p5js.org/odmundeetgen/sketches/qqmp0fVSK
 /* ===
 ml5 Example
 PoseNet example using p5.js
@@ -21,7 +22,11 @@ function setup() {
   video.size(width, height);
 
   // Create a new poseNet method with a single detection
-  poseNet = ml5.poseNet(video, { maxPoseDetections: 1, architecture: "ResNet50"},  modelReady);
+  poseNet = ml5.poseNet(
+    video,
+    { maxPoseDetections: 1, architecture: "ResNet50" },
+    modelReady
+  );
   // This sets up an event that fills the global variable "poses"
   // with an array every time new poses are detected
   poseNet.on("pose", function (results) {
@@ -36,16 +41,15 @@ function modelReady() {
 }
 
 function draw() {
-  background("black")
-    // tint(255, 50); // Display at half opacity
+  background("black");
+  // tint(255, 50); // Display at half opacity
 
   // image(video, 0, 0, width, height);
 
   // We can call both functions to draw all keypoints and the skeletons
-  drawKeypoints();
   drawSkeleton();
+  drawKeypoints();
   // saveCanvas('Img', 'png');
-
 }
 
 function importantKeypoint(keypoint) {
@@ -67,6 +71,29 @@ function importantKeypoint(keypoint) {
   return importantKeypoints.indexOf(keypoint.part) != -1;
 }
 
+const blue = "#0000FF";
+const magenta = "#FF00FF";
+const purple = "#6900ff";
+const allColors = [blue, magenta, purple];
+
+function getRandomColor(colors) {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+function drawRandomColorEllipse(x, y, w, h) {
+  let fillColor = getRandomColor(allColors);
+  fill(fillColor);
+  noStroke();
+  ellipse(x, y, w, h);
+}
+
+function getKeypointDiameter(keypoint) {
+  if (keypoint.part == "nose") {
+    return 100;
+  }
+  return 8;
+}
+
 // A function to draw ellipses over the detected keypoints
 function drawKeypoints() {
   // Loop through all the poses detected
@@ -79,9 +106,13 @@ function drawKeypoints() {
       // Only draw an ellipse is the pose probability is bigger than 0.2
 
       if (keypoint.score > 0.2 && importantKeypoint(keypoint)) {
-        fill(255, 255, 255);
-        noStroke();
-        ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+        keypointDiameter = getKeypointDiameter(keypoint);
+        drawRandomColorEllipse(
+          keypoint.position.x,
+          keypoint.position.y,
+          keypointDiameter,
+          keypointDiameter
+        );
       }
     }
   }
@@ -96,19 +127,30 @@ function drawSkeleton() {
     for (let j = 0; j < skeleton.length; j++) {
       let partA = skeleton[j][0];
       let partB = skeleton[j][1];
+      let x1 = partA.position.x;
+      let y1 = partA.position.y;
+      let x2 = partB.position.x;
+      let y2 = partB.position.y;
       stroke(255, 255, 255);
-      line(
-        partA.position.x,
-        partA.position.y,
-        partB.position.x,
-        partB.position.y
-      );
+      strokeWeight(3);
+      let randomColor1 = getRandomColor(allColors);
+      let randomColor2 = getRandomColor(allColors);
+      drawGradientLine(x1, y1, x2, y2, randomColor1, randomColor2);
     }
   }
 }
 
+function drawGradientLine(x1, y1, x2, y2, color1, color2) {
+  var grad = drawingContext.createLinearGradient(x1, y1, x2, y2);
+  grad.addColorStop(0, color1);
+  grad.addColorStop(1, color2);
+
+  drawingContext.save();
+  drawingContext.strokeStyle = grad;
+
+  line(x1, y1, x2, y2);
+}
 /*
 ideas:
 * save the output into a csv file so i can recreate it later
 */
-
